d8ad495e2ac5148d91238b21f80f876b93ce6493
Mariano Bozzoletti <mbozzoletti@hotmail.com>
2022-07-20 15:54:46 -0300

    Sign Up maquetado y una carpeta nueva para los colores

--- client/App.js
+++ client/App.js
@@ -1,14 +1,32 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { NativeRouter } from "react-router-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import OnBoarding from "./Components/OnBoarding/OnBoarding";
+import Login from "./Components/Login/Login";
+import { Provider } from "react-redux/es/exports";
+import store from "./Redux/Store";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-      <NativeRouter>
-        <View style={styles.container}>
-          <Text>Hola</Text>
-        </View>
-      </NativeRouter>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Onboarding" component={OnBoarding} />
+          {/* <Stack.Screen name='Selection' component={Selection} />
+				<Stack.Screen
+					name='ProfessionalSignUp'
+					component={ProfessionalSignUp}
+				/>
+				<Stack.Screen name='ClientSignUp' component={ClientSignUp} /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
