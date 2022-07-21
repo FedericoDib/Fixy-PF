db0dea550e89bab320a6836bf670f767a3fc8a9a
MarianoRibas <marianoribas98@gmail.com>
2022-07-21 11:49:37 -0300

    Put_fixed

--- api/src/routes/middlewares/request.js
+++ api/src/routes/middlewares/request.js
@@ -6,7 +6,6 @@ const router = Router();
 router.post("/", async (req, res) => {
   const {
     clientId,
-
     affair,
     date,
     description,
@@ -15,41 +14,45 @@ router.post("/", async (req, res) => {
 
   const newRequest = await Request.create({
     clientId,
-
     affair,
     date,
     description,
     status,
   });
 
-  res.send(newRequest);
+  res.send("Solicitud creada con exito");
 });
 
 router.put("/", async (req, res) => {
   const { clientId, associatedProfessionals } = req.body;
 
-  let requestPut = await Request.findOne({
+  const requestPut = await Request.findOne({
     where: { clientId: clientId },
   });
-  // let currentProfesional = requestPut.associatedProfessionals;
-  // // if (!currentProfesional) {
-  // //   const requestArray = [];
-  // //   requestArray.push(associatedProfessionals);
-  // //   let request = await requestPut.update({
-  // //     associatedProfessionals: requestArray,
-  // //   });
-  // // } else {
-  // //   currentProfesional.push(associatedProfessionals);
-  // //   let request = await requestPut.update({
-  // //     associatedProfessionals: currentProfesional,
-  // //   });
-  // // }
-  // currentProfesional.push(associatedProfessionals);
-  // let request = await requestPut.update({
-  //   associatedProfessionals: currentProfesional,
-  // });
+  
+  let currentProfessionals = requestPut.associatedProfessionals;
 
-  res.send(requestPut);
+  if (!currentProfessionals){
+    let newProfessionals = [];
+    newProfessionals.push(associatedProfessionals);
+    const request = await Request.update({
+        associatedProfessionals: newProfessionals
+      }, {where:{
+        clientId: clientId
+      }});
+  } else {
+    if (currentProfessionals.includes(associatedProfessionals)){
+      res.send("Profesional ya asociado")
+    } else {
+      currentProfessionals.push(associatedProfessionals);
+      const request = await Request.update({
+        associatedProfessionals: currentProfessionals
+      }, {where:{
+        clientId: clientId
+      }});
+      res.send("Profesional asociado");
+    }
+  };
 });
 
 module.exports = router;
