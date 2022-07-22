9c806e3e1a49264ebdb53bd2c361ab1beafd7158
Maximiliano Blas Silva <silvamaxiblas@gmail.com>
2022-07-22 11:23:44 -0300

    feat/resume/MaxiSilva

--- client/App.js
+++ client/App.js
@@ -1,40 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import OnBoarding from './Components/OnBoarding/OnBoarding';
-import Login from './Components/Login/Login';
-import { Provider } from 'react-redux/es/exports';
-import store from './Redux/Store';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import OnBoarding from "./Components/OnBoarding/OnBoarding";
+import Login from "./Components/Login/Login";
+import Resume from "./Components/Resume/Resume.jsx";
+import { Provider } from "react-redux/es/exports";
+import store from "./Redux/Store";
 
 export default function App() {
-	const Stack = createNativeStackNavigator();
-	return (
-		<Provider store={store}>
-			<NavigationContainer>
-				<Stack.Navigator>
-					<Stack.Screen
-						name='Login'
-						component={Login}
-						options={{ headerShown: false }}
-					/>
-					<Stack.Screen name='Onboarding' component={OnBoarding} />
-					{/* <Stack.Screen name='Selection' component={Selection} />
+    const Stack = createNativeStackNavigator();
+    return (
+        <Provider store={store}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Resume"
+                        component={Resume}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="Login"
+                        component={Login}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen name="Onboarding" component={OnBoarding} />
+                    {/* <Stack.Screen name='Selection' component={Selection} />
 					<Stack.Screen
 						name='ProfessionalSignUp'
 						component={ProfessionalSignUp}
 					/>
 					<Stack.Screen name='ClientSignUp' component={ClientSignUp} /> */}
-				</Stack.Navigator>
-			</NavigationContainer>
-		</Provider>
-	);
+                </Stack.Navigator>
+            </NavigationContainer>
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
---
+++ client/Components/Resume/Resume.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    TouchableHighlight,
+    Alert,
+    ScrollView,
+} from "react-native";
+import IconCalendar from "react-native-vector-icons/EvilIcons";
+import IconPhone from "react-native-vector-icons/Feather";
+import { useNavigation } from "@react-navigation/native";
+import style from "./ResumeStyle";
+
+export default function Resume() {
+    const navigation = useNavigation();
+
+    return (
+        <ScrollView style={style.mainContainer}>
+            <View style={style.calendarPhoneContainer}>
+                <View style={style.calendarContainer}>
+                    <Text>15/09/2022</Text>
+                    <IconCalendar
+                        name="calendar"
+                        size={30}
+                        color="#1a57d2"
+                    ></IconCalendar>
+                </View>
+                <View style={style.phoneContainer}>
+                    <IconPhone
+                        onPress={() => {
+                            Alert.alert("Llamado");
+                        }}
+                        name="phone-call"
+                        size={30}
+                        color="green"
+                    ></IconPhone>
+                </View>
+            </View>
+            <View style={style.textContainer}>
+                <Text>Horario</Text>
+                <View style={style.centerField}>
+                    <Text>09:30 hs</Text>
+                </View>
+            </View>
+
+            <View style={style.textContainer}>
+                <Text>Presupuesto Aproximado</Text>
+            </View>
+            <View style={style.endField}>
+                <Text>$ 2.000</Text>
+            </View>
+            <View style={style.textContainer}>
+                <Text>Metodo de pago</Text>
+                <View style={style.centerField}>
+                    <Text>Paypal</Text>
+                </View>
+            </View>
+            <View style={style.textContainer}>
+                <Text>Descripcion del problema</Text>
+                <View style={style.centerField}>
+                    <Text>
+                        Lorem ipsum es el texto que se usa habitualmente en
+                        diseño gráfico en demostraciones de tipografías o de
+                        borradores de diseño para probar el diseño visual antes
+                        de insertar el texto final.
+                    </Text>
+                </View>
+            </View>
+            <View style={style.textContainer}>
+                <Text>Comentarios</Text>
+                <View style={style.centerField}>
+                    <Text>
+                        Lorem ipsum es el texto que se usa habitualmente en
+                        diseño gráfico en demostraciones de tipografías o de
+                        borradores de diseño para probar el diseño visual antes
+                        de insertar el texto final.
+                    </Text>
+                </View>
+            </View>
+            <TouchableHighlight
+                style={style.button}
+                activeOpacity={0.6}
+                underlayColor="#F9CE67"
+                onPress={() => {
+                    navigation.navigate("Onboarding");
+                }}
+            >
+                <View style={style.textButton}>
+                    <Text>Visita Finalizada</Text>
+                </View>
+            </TouchableHighlight>
+        </ScrollView>
+    );
+}
