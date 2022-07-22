9f57422a19ceb64d7bf9f79084448d38aa8b20be
Javier Martinez Blanco <jh.martinezblanco@gmail.com>
2022-07-22 14:51:25 -0300

    relacion de tablas y rutas get
    i dont know

--- api/src/models/Request.js
+++ api/src/models/Request.js
@@ -1,34 +1,37 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("request", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-      allowNull: false,
+  sequelize.define(
+    "request",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      affair: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      date: {
+        type: DataTypes.STRING,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.STRING,
+        defaultValue: "pending",
+      },
+      budget: {
+        type: DataTypes.ARRAY(DataTypes.JSON),
+      },
     },
-    clientId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    affair: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    date: {
-      type: DataTypes.STRING,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.ENUM("declined", "pending", "accepted", "finished"),
-      defaultValue: "pending",
-    },
-    budget: {
-      type: DataTypes.ARRAY(DataTypes.JSON),
-    },
-  });
+    {
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
 };
