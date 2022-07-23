dc62032a4e8f616edbb70183999a2a32b5e7297e
Javier Martinez Blanco <jh.martinezblanco@gmail.com>
2022-07-23 00:23:42 -0300

    db hc a api
    pase la db hc a api y puse la ruta get y la action

--- client/Redux/Action/index.js
+++ client/Redux/Action/index.js
@@ -1,34 +1,36 @@
-import axios from 'axios';
-export const GOOGLE_LOGIN = 'GOOGLE_LOGIN';
-export const GET_ALL = 'GET_ALL';
+import axios from "axios";
+export const GOOGLE_LOGIN = "GOOGLE_LOGIN";
+export const GET_ALL = "GET_ALL";
 
-import db from '../../db.hardcode.json';
+// import db from "../../db.hardcode.json";
 
 export const googleLogin = (id) => {
-	return async (dispatch) => {
-		let response = await axios.get(
-			`http://192.68.40.173:3000/userInfo?id=${id}`
-		);
-		return dispatch({
-			type: GOOGLE_LOGIN,
-			payload: response.data,
-		});
-	};
+  return async (dispatch) => {
+    let response = await axios.get(
+      `http://192.68.40.173:3000/userInfo?id=${id}`
+    );
+    return dispatch({
+      type: GOOGLE_LOGIN,
+      payload: response.data,
+    });
+  };
 };
 
-// export const getAllProfessionals = () => {
-//   return async (dispatch) => {
-//     const info = await axios.get("http://192.168.0.11:3000/type/all");
-//     return dispatch({
-//       type: GET_ALL,
-//       payload: db.professional
-//     });
-//   };
+export const getAllProfessionals = (profession) => {
+  return async (dispatch) => {
+    const info = await axios.get(
+      `http://192.168.0.11:3000/professional?profession=${profession}`
+    );
+    return dispatch({
+      type: GET_ALL,
+      payload: info.data,
+    });
+  };
+};
+
+// export const getAllProfessionalsJson = () => {
+// 	return {
+// 		type: GET_ALL,
+// 		payload: db.professional,
+// 	};
 // };
-
-export const getAllProfessionalsJson = () => {
-	return {
-		type: GET_ALL,
-		payload: db.professional,
-	};
-};
