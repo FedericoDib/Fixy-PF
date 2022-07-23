53bb605fd9f274b87541339bf8d87c19449b2341
Mariano Bozzoletti <mbozzoletti@hotmail.com>
2022-07-23 15:12:38 -0300

    Cambios en Onboarding

--- client/Components/OnBoarding/OnBoarding.jsx
+++ client/Components/OnBoarding/OnBoarding.jsx
@@ -1,11 +1,21 @@
 import React, { useRef, useState } from "react";
-import { View, Text, FlatList, StyleSheet, Animated } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Animated,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
 import slides from "./slides";
 import OnBoardingItem from "./OnBoardingItem";
 import Paginator from "./Paginator";
+import { useNavigation } from "@react-navigation/native";
 
 export default function OnBoarding() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const navigation = useNavigation();
 
   const viewableItemsChangue = useRef(({ viewableItems }) => {
     setCurrentIndex(viewableItems[0].index);
@@ -15,42 +25,85 @@ export default function OnBoarding() {
   const slidesRef = useRef(null);
   const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
 
+  const scrollTo = () => {
+    if (currentIndex < slides.length - 1) {
+      slidesRef.current.scrollToIndex({ index: currentIndex + 1 });
+    } else {
+      Alert.alert("Estas en la ultima slide");
+    }
+  };
   return (
     <View style={style.container}>
-      <FlatList
-        data={slides}
-        renderItem={({ item }) => <OnBoardingItem item={item} />}
-        horizontal
-        pagingEnabled
-        bounces={false}
-        onScroll={Animated.event(
-          [
-            {
-              nativeEvent: {
-                contentOffset: {
-                  x: scrollX,
+      <View style={{ flex: 18 }}>
+        <FlatList
+          data={slides}
+          renderItem={({ item }) => <OnBoardingItem item={item} />}
+          horizontal
+          pagingEnabled
+          bounces={false}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: scrollX,
+                  },
                 },
               },
-            },
-          ],
-          {
-            useNativeDriver: false,
-          }
-        )}
-        onViewableItemsChanged={viewableItemsChangue}
-        viewabilityConfig={viewConfig}
-        ref={slidesRef}
-      />
-
-      <Paginator data={slides} scrollX={scrollX} />
+            ],
+            {
+              useNativeDriver: false,
+            }
+          )}
+          onViewableItemsChanged={viewableItemsChangue}
+          viewabilityConfig={viewConfig}
+          ref={slidesRef}
+        />
+      </View>
+      <View style={{ flex: 2 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            backgroundColor: "green",
+            flex: 2,
+          }}
+        >
+          <TouchableOpacity
+            style={[
+              style.btnNyP,
+              {
+                backgroundColor: "transparent",
+                borderWidth: 1,
+                borderColor: "#493d8a",
+              },
+            ]}
+          >
+            <Text
+              style={{ color: "#493d8a", fontWeight: "bold", fontSize: 18 }}
+            >
+              Skip
+            </Text>
+          </TouchableOpacity>
+          <View style={{ width: 20 }}></View>
+          <TouchableOpacity onPress={() => scrollTo()} style={style.btnNyP}>
+            <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 18 }}>
+              Next
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ flex: 1 }}>
+          <Paginator data={slides} scrollX={scrollX} />
+        </View>
+      </View>
     </View>
   );
 }
 
 const style = StyleSheet.create({
   container: {
-    flex: 3,
     backgroundColor: "#fff",
     paddingBottom: 55,
+    flex: 1,
   },
 });
--- client/Components/OnBoarding/OnBoardingItem.jsx
+++ client/Components/OnBoarding/OnBoardingItem.jsx
@@ -13,7 +13,7 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 export default function OnBoardingItem({ item }) {
   const { width, height } = useWindowDimensions();
   return (
-    <View style={[styles.container, { width, height }]}>
+    <View style={[styles.container, { width }]}>
       <Text style={styles.title}>{item.title}</Text>
       <Image
         source={item.image}
@@ -21,28 +21,6 @@ export default function OnBoardingItem({ item }) {
       />
       <View style={{ flex: 0.3 }}>
         <Text style={styles.description}>{item.description}</Text>
-      </View>
-      <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
-        <TouchableOpacity
-          style={[
-            styles.btnNyP,
-            {
-              backgroundColor: "transparent",
-              borderWidth: 1,
-              borderColor: "#493d8a",
-            },
-          ]}
-        >
-          <Text style={{ color: "#493d8a", fontWeight: "bold", fontSize: 18 }}>
-            Skip
-          </Text>
-        </TouchableOpacity>
-        <View style={{ width: 20 }}></View>
-        <TouchableOpacity style={styles.btnNyP}>
-          <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 18 }}>
-            Next
-          </Text>
-        </TouchableOpacity>
       </View>
     </View>
   );
@@ -54,11 +32,13 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   container: {
-    flex: 1,
     justifyContent: "center",
     alignItems: "center",
     paddingTop: 60,
     backgroundColor: "transparent",
+    backgroundColor: "yellow",
+    height: 800,
+    marginBottom: 200,
   },
   title: {
     fontWeight: "800",
@@ -74,6 +54,7 @@ const styles = StyleSheet.create({
     marginTop: 45,
     fontSize: 20,
     color: "#62656b",
+    backgroundColor: "red",
   },
   btnNyP: {
     backgroundColor: "#493d8a",
