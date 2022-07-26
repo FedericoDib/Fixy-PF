5cec63d3d4585579378fdff094130cc90d3e2e58
Federico Dib <68966481+FedericoDib@users.noreply.github.com>
2022-07-25 23:43:16 -0300

    half-merge

--- client/Components/List/List.jsx
+++ client/Components/List/List.jsx
@@ -7,52 +7,54 @@ import { useSelector, useDispatch } from "react-redux";
 import { getAllProfessionals, searchProfessional } from "../../Redux/Action";
 
 export default function List({ navigation }) {
-  const [inputSearch, setInputSearch] = useState("");
-  const [filterData, setFilterData] = useState([]);
-  const professionals = useSelector((state) => state.professionals);
-  const dispatch = useDispatch();
+    const [inputSearch, setInputSearch] = useState("");
+    const [filterData, setFilterData] = useState([]);
+    const professionals = useSelector((state) => state.professionals);
+    const dispatch = useDispatch();
 
-  // let newdata = [];
-  // let dataDefault = [
-  //   {
-  //     name: "Profesional no encontrado",
-  //   },
-  // ];
-  // function onChange(e) {
-  //   setInputSearch(e.text);
-  //   newdata = professionals.filter((user) => user.name.includes(e.text));
-  //   setFilterData(newdata);
-  // }
-  const handleChange = (e) => {
-    dispatch(searchProfessional(e.text));
-  };
-  return (
-    <View style={style.mainContainer}>
-      <View>
-        <TextInput
-          onChangeText={(text) => handleChange({ text })}
-          label="Buscar"
-          variant="outlined"
-          // trailing={(props) => (
-          //     <IconButton
-          //         icon={(props) => <Icon name="search" {...props} />}
-          //         {...props}
-          //     />
-          // )}
-        />
-      </View>
-      <FlatList
-        data={
-          filterData.length
-            ? filterData
-            : inputSearch.length
-            ? dataDefault
-            : professionals
-        }
-        renderItem={({ item }) => (
-          <CardList navigation={navigation} item={item} />
-        )}
-      />
-    </View>
-  );
+    // let newdata = [];
+    // let dataDefault = [
+    //   {
+    //     name: "Profesional no encontrado",
+    //   },
+    // ];
+    // function onChange(e) {
+    //   setInputSearch(e.text);
+    //   newdata = professionals.filter((user) => user.name.includes(e.text));
+    //   setFilterData(newdata);
+    // }
+    const handleChange = (e) => {
+        dispatch(searchProfessional(e.text));
+    };
+    return (
+        <View style={style.mainContainer}>
+            <View style={{ flex: 1 }}>
+                <TextInput
+                    onChangeText={(text) => handleChange({ text })}
+                    label="Buscar"
+                    variant="outlined"
+                    // trailing={(props) => (
+                    //     <IconButton
+                    //         icon={(props) => <Icon name="search" {...props} />}
+                    //         {...props}
+                    //     />
+                    // )}
+                />
+            </View>
+            <View style={{ flex: 10 }}>
+                <FlatList
+                    data={
+                        filterData.length
+                            ? filterData
+                            : inputSearch.length
+                            ? dataDefault
+                            : professionals
+                    }
+                    renderItem={({ item }) => (
+                        <CardList navigation={navigation} item={item} />
+                    )}
+                />
+            </View>
+        </View>
+    );
 }
