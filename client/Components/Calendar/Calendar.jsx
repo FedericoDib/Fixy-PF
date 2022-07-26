dbd29fd980ff68e69d395ea47ebafa5a0044c709
Federico Dib <68966481+FedericoDib@users.noreply.github.com>
2022-07-26 14:16:29 -0300

    feat/half-calendar/fede-dib

--- client/Components/Calendar/Calendar.jsx
+++ client/Components/Calendar/Calendar.jsx
@@ -1,12 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import {Calendar} from 'react-native-calendars' 
+// import ApiCalendar from 'react-google-calendar-api';
 
-const Calendar = () => {
+// const config = {
+//   "clientId": "302940809798-bb9fvtjipv232sglpnrglc228fp28r1q.apps.googleusercontent.com",
+//   "apiKey": "AIzaSyDF0_LtJF7Ct04VBorVRloJ-MB1Ofj2OHY",
+//   "scope": "https://www.googleapis.com/auth/calendar",
+//   "discoveryDocs": [
+//     "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
+//   ]
+// }
+
+// const apiCalendar = new ApiCalendar(config)
+
+const CalendarView = () => {
   return (
-    <View>
-      <Text>Calendar</Text>
-    </View>
+    <>
+      <Calendar onDayPress={(e) => {console.log('aca iria el ver evento', e)}} enableSwipeMonths style={{height: "100%", paddingTop: 100}} />
+    </>
   )
 }
 
-export default Calendar
+export default CalendarView
