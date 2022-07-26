c82b11f767bda559c82e758acf1532df67e417f2
Federico Dib <68966481+FedericoDib@users.noreply.github.com>
2022-07-25 23:44:10 -0300

    Update ListStyle.js

--- client/Components/List/ListStyle.js
+++ client/Components/List/ListStyle.js
@@ -3,8 +3,11 @@ import { StyleSheet } from "react-native";
 const style = StyleSheet.create({
     mainContainer: {
         backgroundColor: "#EEECF7",
-        marginTop: 50,
+        // marginTop: 40,
+        // marginBottom: 55,
         marginHorizontal: 10,
+        flex: 1,
+        marginTop: 35,
     },
 });
 
