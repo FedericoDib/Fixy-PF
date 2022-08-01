9dfba7ec5ae48eeecec0db144353b4e908f3ff8b
Federico Dib <dibfede@gmail.com>
2022-07-31 22:07:18 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -10,13 +10,44 @@ const modelBudget = require('./models/Budget');
 
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/fixyHome`,
-	{
-		logging: false,
-		native: false,
-	}
-);
+// const sequelize = new Sequelize(
+// 	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/fixyHome`,
+// 	{
+// 		logging: false,
+// 		native: false,
+// 	}
+// );
+
+let sequelize =
+	process.env.NODE_ENV === 'production'
+		? new Sequelize({
+				database: DB_NAME,
+				dialect: 'postgres',
+				host: DB_HOST,
+				port: 5432,
+				username: DB_USER,
+				password: DB_PASSWORD,
+				pool: {
+					max: 3,
+					min: 1,
+					idle: 10000,
+				},
+				dialectOptions: {
+					ssl: {
+						require: true,
+						rejectUnauthorized: false,
+					},
+					keepAlive: true,
+				},
+				ssl: true,
+		  })
+		: new Sequelize(
+				`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/fixyHome`,
+				{
+					logging: false, // set to console.log to see the raw SQL queries
+					native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+				}
+		  );
 const basename = path.basename(__filename);
 
 modelClient(sequelize);
