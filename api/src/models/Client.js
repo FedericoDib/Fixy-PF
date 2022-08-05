8239b3b509079e5ab74231aa7e44e205f73bd505
Federico Dib <dibfede@gmail.com>
2022-08-05 19:15:42 -0300

    Update Client.js

--- api/src/models/Client.js
+++ api/src/models/Client.js
@@ -1,73 +1,73 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-    sequelize.define(
-        "client",
-        {
-            googleId: {
-                type: DataTypes.STRING,
-                primaryKey: true,
-                allowNull: true,
-            },
-            expoToken: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            isRegistered: {
-                type: DataTypes.BOOLEAN,
-            },
-            firstLogin: {
-                type: DataTypes.BOOLEAN,
-                defaultValue: true,
-            },
-            email: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            name: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            phoneNumber: {
-                type: DataTypes.STRING,
-                defaultValue: "-",
-                allowNull: false,
-            },
-            perfilPic: {
-                type: DataTypes.STRING,
-                defaultValue: "-",
-            },
-            province: {
-                type: DataTypes.STRING,
-                defaultValue: "-",
-                allowNull: false,
-            },
-            city: {
-                type: DataTypes.STRING,
-                defaultValue: "-",
-                allowNull: false,
-            },
-            address: {
-                type: DataTypes.STRING,
-                defaultValue: "-",
-                allowNull: false,
-            },
-            reviews: {
-                type: DataTypes.ARRAY(DataTypes.JSON),
-                defaultValue: [],
-            },
-            latitude: {
-                type: DataTypes.FLOAT,
-                defaultValue: "-",
-            },
-            longitude: {
-                type: DataTypes.FLOAT,
-                defaultValue: "-",
-            },
-        },
-        {
-            createdAt: false,
-            updatedAt: false,
-        }
-    );
+	sequelize.define(
+		'client',
+		{
+			googleId: {
+				type: DataTypes.STRING,
+				primaryKey: true,
+				allowNull: true,
+			},
+			expoToken: {
+				type: DataTypes.STRING,
+				//allowNull: false,
+			},
+			isRegistered: {
+				type: DataTypes.BOOLEAN,
+			},
+			firstLogin: {
+				type: DataTypes.BOOLEAN,
+				defaultValue: true,
+			},
+			email: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			phoneNumber: {
+				type: DataTypes.STRING,
+				defaultValue: '-',
+				allowNull: false,
+			},
+			perfilPic: {
+				type: DataTypes.STRING,
+				defaultValue: '-',
+			},
+			province: {
+				type: DataTypes.STRING,
+				defaultValue: '-',
+				allowNull: false,
+			},
+			city: {
+				type: DataTypes.STRING,
+				defaultValue: '-',
+				allowNull: false,
+			},
+			address: {
+				type: DataTypes.STRING,
+				defaultValue: '-',
+				allowNull: false,
+			},
+			reviews: {
+				type: DataTypes.ARRAY(DataTypes.JSON),
+				defaultValue: [],
+			},
+			latitude: {
+				type: DataTypes.FLOAT,
+				defaultValue: '-',
+			},
+			longitude: {
+				type: DataTypes.FLOAT,
+				defaultValue: '-',
+			},
+		},
+		{
+			createdAt: false,
+			updatedAt: false,
+		}
+	);
 };
