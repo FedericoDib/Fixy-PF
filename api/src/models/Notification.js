62a03efee9bc3df1153194bc6b040e59d64c9f43
Javier Martinez Blanco <jh.martinezblanco@gmail.com>
2022-08-07 15:14:47 -0300

    en el postman anda

--- api/src/models/Notification.js
+++ api/src/models/Notification.js
@@ -2,28 +2,28 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "notification",{
-        id:{
-            type: DataTypes.UUIDV1,
-            primaryKey:true,
-            defaultValue: DataTypes.UUIDV4
-        },
-        title:{
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        body:{
-            type: DataTypes.STRING,
-        },
-        status:{
-            type:DataTypes.STRING,
-            defaultValue:'not_seen'
-        }
-
+    "notification",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      body: {
+        type: DataTypes.STRING,
+      },
+      status: {
+        type: DataTypes.STRING,
+        defaultValue: "not_seen",
+      },
     },
     {
-        createdAt: false,
-        updatedAt: false,
-      }
+      createdAt: false,
+      updatedAt: false,
+    }
   );
 };
--- api/src/routes/middlewares/request.js
+++ api/src/routes/middlewares/request.js
@@ -1,273 +1,277 @@
-const { Router, request } = require('express');
-const { Professional, Request, Client, Budget,Notification } = require('../../db');
-const { Expo } = require('expo-server-sdk');
+const { Router, request } = require("express");
+const {
+  Professional,
+  Request,
+  Client,
+  Budget,
+  Notification,
+} = require("../../db");
+const { Expo } = require("expo-server-sdk");
 
 const router = Router();
 
-const db = require('../../db.hardcode.json');
+const db = require("../../db.hardcode.json");
 
 let expo = new Expo();
 //
-router.post('/', async (req, res) => {
-	const {
-		clientId,
-		affair,
-		date,
-		description,
-		address,
-		status,
-		availableTime,
-		category,
-	} = req.body;
+router.post("/", async (req, res) => {
+  const {
+    clientId,
+    affair,
+    date,
+    description,
+    address,
+    status,
+    availableTime,
+    category,
+  } = req.body;
 
-	try {
-		let newRequest = await Request.create({
-			clientId,
-			affair,
-			date,
-			description,
-			status,
-			address,
-			availableTime,
-			category,
-		});
+  try {
+    let newRequest = await Request.create({
+      clientId,
+      affair,
+      date,
+      description,
+      status,
+      address,
+      availableTime,
+      category,
+    });
 
-		res.status(201).send(newRequest);
-	} catch (error) {
-		res.status(400).send('faltan datos');
-	}
+    res.status(201).send(newRequest);
+  } catch (error) {
+    res.status(400).send("faltan datos");
+  }
 });
 
 // SE NOTIFICA AL PROF Q SE ACEPTO UN PRESUPUESTO
-router.put('/:id', async (req, res) => {
-	console.log('idss')
-	let code = Math.floor(Math.random() * (9999 - 1000) + 1000);
-	try {
-		const budget = await Budget.findOne({ where: { id: req.params.id } });
-		await budget.update({ status: 'active', validationCode: code });
-		const professional = await Professional.findOne({
-			where: { googleId: budget.professionalId },
-		});
-		console.log('dshajk', professional);
-		const client = await Client.findOne({
-			where: { googleId: budget.clientId },
-		});
-		const reqst = await Request.findOne({ where: { id: budget.requestId } });
-		await Request.update(
-			{ status: 'active' },
-			{ where: { id: budget.requestId } }
-		);
+router.put("/:id", async (req, res) => {
+  console.log("idss");
+  let code = Math.floor(Math.random() * (9999 - 1000) + 1000);
+  try {
+    const budget = await Budget.findOne({ where: { id: req.params.id } });
+    await budget.update({ status: "active", validationCode: code });
+    const professional = await Professional.findOne({
+      where: { googleId: budget.professionalId },
+    });
+    console.log("dshajk", professional);
+    const client = await Client.findOne({
+      where: { googleId: budget.clientId },
+    });
+    const reqst = await Request.findOne({ where: { id: budget.requestId } });
+    await Request.update(
+      { status: "active" },
+      { where: { id: budget.requestId } }
+    );
 
-		//NOTIFICACION AL PROFESIONAL DEL BUDGET ACEPTADO
-		const expoPushToken = professional.expoToken;
+    //NOTIFICACION AL PROFESIONAL DEL BUDGET ACEPTADO
+    const expoPushToken = professional.expoToken;
 
-		let messages = [];
+    let messages = [];
 
-		messages.push({
-			to: expoPushToken,
-			sound: 'default',
-			body: `${client.name} ha aceptado tu presupuesto para el problema: ${reqst.affair}`,
-			data: { withSome: 'data' },
-		});
+    messages.push({
+      to: expoPushToken,
+      sound: "default",
+      body: `${client.name} ha aceptado tu presupuesto para el problema: ${reqst.affair}`,
+      data: { withSome: "data" },
+    });
 
+    let chunks = expo.chunkPushNotifications(messages);
+    let tickets = [];
+    (async () => {
+      // Send the chunks to the Expo push notification service. There are
+      // different strategies you could use. A simple one is to send one chunk at a
+      // time, which nicely spreads the load out over time:
+      for (let chunk of chunks) {
+        try {
+          let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+          // console.log('tickettttt',ticketChunk);
+          tickets.push(...ticketChunk);
+          // NOTE: If a ticket contains an error code in ticket.details.error, you
+          // must handle it appropriately. The error codes are listed in the Expo
+          // documentation:
+          // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    })();
 
-		let chunks = expo.chunkPushNotifications(messages);
-		let tickets = [];
-		(async () => {
-			// Send the chunks to the Expo push notification service. There are
-			// different strategies you could use. A simple one is to send one chunk at a
-			// time, which nicely spreads the load out over time:
-			for (let chunk of chunks) {
-				try {
-					let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-					// console.log('tickettttt',ticketChunk);
-					tickets.push(...ticketChunk);
-					// NOTE: If a ticket contains an error code in ticket.details.error, you
-					// must handle it appropriately. The error codes are listed in the Expo
-					// documentation:
-					// https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
-				} catch (error) {
-					console.error(error);
-				}
-			}
-		})();
+    //GUARDADO EN DB DE LA NOTIF ENVIADA
 
-		//GUARDADO EN DB DE LA NOTIF ENVIADA
+    // const newNotifDb = await Notification.create({
+    // 	title:messages[0].body,
+    // 	clientId:client.googleId,
+    // 	professionalId:professional.googleId
+    // })
 
-		// const newNotifDb = await Notification.create({
-		// 	title:messages[0].body,
-		// 	clientId:client.googleId,
-		// 	professionalId:professional.googleId
-		// })
-
-
-		res.status(202).send('ok');
-	} catch (error) {
-		res.status(400).send(error);
-	}
+    res.status(202).send("ok");
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-router.put('/h', async (req, res) => {
-	const { googleId, idRequest } = req.body;
-	
-	try {
-		const request = await Request.findOne({ where: { id: idRequest } });
-		const professional = await Professional.findOne({
-			where: { googleId },
-		});
-		const client = await Client.findOne({where:{googleId:request.clientId}})
+router.put("/", async (req, res) => {
+  const { googleId, idRequest } = req.body;
 
-		await request.addProfessional(professional);
+  try {
+    const request = await Request.findOne({ where: { id: idRequest } });
+    const professional = await Professional.findOne({
+      where: { googleId },
+    });
+    const client = await Client.findOne({
+      where: { googleId: request.clientId },
+    });
 
-		// NOTIFICATIONS
-		const expoPushToken = professional.expoToken;
+    await request.addProfessional(professional);
 
-		let messages = [];
+    // NOTIFICATIONS
+    const expoPushToken = professional.expoToken;
 
-		messages.push({
-			to: expoPushToken,
-			sound: 'default',
-			body: 'Tenes una nueva solicitud de presupuesto',
-			data: { withSome: 'data' },
-		});
+    let messages = [];
 
-		let chunks = expo.chunkPushNotifications(messages);
-		let tickets = [];
-		(async () => {
-			// Send the chunks to the Expo push notification service. There are
-			// different strategies you could use. A simple one is to send one chunk at a
-			// time, which nicely spreads the load out over time:
-			for (let chunk of chunks) {
-				try {
-					let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-					// console.log('tickettttt',ticketChunk);
-					tickets.push(...ticketChunk);
-					// NOTE: If a ticket contains an error code in ticket.details.error, you
-					// must handle it appropriately. The error codes are listed in the Expo
-					// documentation:
-					// https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
-				} catch (error) {
-					console.error(error);
-				}
-			}
-		})();
+    messages.push({
+      to: expoPushToken,
+      sound: "default",
+      body: "Tenes una nueva solicitud de presupuesto",
+      data: { withSome: "data" },
+    });
 
-		
+    let chunks = expo.chunkPushNotifications(messages);
+    let tickets = [];
+    (async () => {
+      // Send the chunks to the Expo push notification service. There are
+      // different strategies you could use. A simple one is to send one chunk at a
+      // time, which nicely spreads the load out over time:
+      for (let chunk of chunks) {
+        try {
+          let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+          // console.log('tickettttt',ticketChunk);
+          tickets.push(...ticketChunk);
+          // NOTE: If a ticket contains an error code in ticket.details.error, you
+          // must handle it appropriately. The error codes are listed in the Expo
+          // documentation:
+          // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    })();
 
-		// const newNotifDb = await Notification.create({
-		// 	title:messages[0].body,
-		// 	clientId:client.googleId,
-		// 	professionalId:professional.googleId
-		// });
+    const newNotifDb = await Notification.create({
+      title: messages[0].body,
+      clientId: client.googleId,
+      professionalId: professional.googleId,
+    });
 
-		// if(newNotifDb) console.log("OK NOTIF DB");
+    if (newNotifDb) console.log("OK NOTIF DB");
 
-		//----------------------//
-		res.status(200).send('Solicitud creada con exito');
-	} catch (error) {
-		res.status(400).send(error);
-	}
+    //----------------------//
+    res.status(200).send("Solicitud creada con exito");
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-router.get('/', async (req, res) => {
-	const { id } = req.query;
-	try {
-		let request;
-		if (id) {
-			request = await Request.findOne({
-				where: { id },
-				include: [{ model: Professional }],
-			});
-			if (request) {
-				res.status(200).send(request);
-			} else {
-				res.status(400).send('no existe la request');
-			}
-		} else {
-			request = await Request.findAll();
-			res.send(request);
-		}
-	} catch (error) {
-		res.status(400).send(error);
-	}
+router.get("/", async (req, res) => {
+  const { id } = req.query;
+  try {
+    let request;
+    if (id) {
+      request = await Request.findOne({
+        where: { id },
+        include: [{ model: Professional }],
+      });
+      if (request) {
+        res.status(200).send(request);
+      } else {
+        res.status(400).send("no existe la request");
+      }
+    } else {
+      request = await Request.findAll();
+      res.send(request);
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-router.get('/professional', async (req, res) => {
-	const { id } = req.query;
+router.get("/professional", async (req, res) => {
+  const { id } = req.query;
 
-	try {
-		const requests = await Professional.findOne({
-			where: {
-				googleId: id,
-			},
-			include: [
-				{
-					model: Request,
-					attributes: [
-						'affair',
-						'description',
-						'date',
-						'address',
-						'clientId',
-						'status',
-						'id',
-						'availableTime',
-					],
-				},
-			],
-			attributes: ['name'],
-		});
-		if (!request) {
-			res.status(400).send('no existe ese profesional');
-		} else {
-			res.status(200).send(requests.requests);
-		}
-	} catch (error) {
-		res.status(400).send(error);
-	}
+  try {
+    const requests = await Professional.findOne({
+      where: {
+        googleId: id,
+      },
+      include: [
+        {
+          model: Request,
+          attributes: [
+            "affair",
+            "description",
+            "date",
+            "address",
+            "clientId",
+            "status",
+            "id",
+            "availableTime",
+          ],
+        },
+      ],
+      attributes: ["name"],
+    });
+    if (!request) {
+      res.status(400).send("no existe ese profesional");
+    } else {
+      res.status(200).send(requests.requests);
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 // MUESTRA LOS CLIENTES ASOCIADOS A LA REQUEST
 
-router.get('/client', async (req, res) => {
-	const { id } = req.query;
-	try {
-		const requests = await Client.findAll({
-			where: {
-				googleId: id,
-			},
-			include: [
-				{
-					model: Request,
-					as: 'requests',
-				},
-			],
-			attributes: ['name'],
-		});
-		if (request) {
-			res.status(200).send(requests[0].requests);
-		} else {
-			res.status(400).send('no existe el cliente');
-		}
-	} catch (error) {}
+router.get("/client", async (req, res) => {
+  const { id } = req.query;
+  try {
+    const requests = await Client.findAll({
+      where: {
+        googleId: id,
+      },
+      include: [
+        {
+          model: Request,
+          as: "requests",
+        },
+      ],
+      attributes: ["name"],
+    });
+    if (request) {
+      res.status(200).send(requests[0].requests);
+    } else {
+      res.status(400).send("no existe el cliente");
+    }
+  } catch (error) {}
 });
 
-router.delete('/:id', async (req, res) => {
-	const { id } = req.params;
-	try {
-		const requestDelete = await Request.findByPk(id);
-		const client = await Client.findByPk(requestDelete.clientId);
-		await requestDelete.destroy();
-		const requests = await Client.findOne({
-			where: {
-				googleId: client.googleId,
-			},
-			include: [{ model: Request, as: 'requests' }],
-		});
-		res.status(200).send(requests.requests);
-	} catch (error) {
-		res.status(400).send(error);
-	}
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const requestDelete = await Request.findByPk(id);
+    const client = await Client.findByPk(requestDelete.clientId);
+    await requestDelete.destroy();
+    const requests = await Client.findOne({
+      where: {
+        googleId: client.googleId,
+      },
+      include: [{ model: Request, as: "requests" }],
+    });
+    res.status(200).send(requests.requests);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;
